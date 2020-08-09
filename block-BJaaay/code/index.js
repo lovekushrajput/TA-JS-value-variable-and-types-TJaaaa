788161e403a4e19e4b66d0dad1562a5815b3160c
Ankit <iankit17@gmail.com>
2020-08-09 12:56:36 +0530

    add block-BJaaay

---
+++ block-BJaaay/code/index.js
@@ -0,0 +1,13 @@
+//  Loop basics
+
+// Do the following using loops:
+
+// Using `console.log` log all the values from 1 to 10.
+
+// Using `console.log` log all the evan values from 1 to 10.
+
+// Using `console.log` log all the od values from 1 to 10.
+
+// Calculate the sum of all numbers from 1 to 10.
+
+// Log all the values from 1 to 10 using while loop
