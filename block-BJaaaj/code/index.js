2c7d425a9ad8bb1fba74d4796668928610ef93b9
Ankit <iankit17@gmail.com>
2020-08-09 12:56:36 +0530

    add block-BJaaaj

---
+++ block-BJaaaj/code/index.js
@@ -0,0 +1,41 @@
+// Accessing and assigning.
+
+let firstName = "Arya";
+let lastName = "Stark";
+
+// Do the following:
+
+// Alert the value of the firstName.
+// Alert the value of the lastName.
+// Assign a new value of `"John"` to firstName.
+// Assign a new value of `"Snow"` to lastName.
+// Alert the new value of `firstName` and `lastName` by concatenating them
+// Create a new varaible using `const` named `fullName` and assign the fullname using firstName and lastName seperated by space.
+
+// Declare three variables `name,` `newUser` and `isAdmin`.
+// Assign your name as the value of `name`.
+// Assign the value of `name` to `newUser`.
+// Set the value of `isAdmin` to `true`.
+// Alert the values of all the three variables.
+
+// Assignment and re-assignment
+
+var wiseMan = "Tyrion Lannister";
+
+// Follow instructions and do the following. In case of an error write the error in comment
+
+// Reassign the value of `wiseMan` to "Samwell Tarly"
+// Declare a variable `userName` with value "Lysa Arryn"
+// Declare a variable named `oddNumber` and assign a value `57`.
+// Reassign the value of `oddNumber` to 61
+
+// Mathematical Operation
+
+let amount = 4280;
+
+// Using mathematical operations find the solutions. `(+, -, \*, / , etc.)`
+
+// Declare a new variable `reducedAmount`. In it store the value that is 24 less than the value of amount.
+// Declare another variable `addedAmount`. It's value should be 32 more than the value of amount.
+// Declare a variable `multipleAmount`. It's value should be 7 times the value of amount.
+// Declare a variable `dividedAmount`. It should store the resultant of amount divided by 57.
