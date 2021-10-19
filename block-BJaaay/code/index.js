24fccf39bb8a79073532ac1f256793aa3ac5ee6d
lovekush rajput <lovekushrazput143@gmail.com>
2021-10-19 10:02:35 +0530

    completed

--- block-BJaaay/code/index.js
+++ block-BJaaay/code/index.js
@@ -3,32 +3,34 @@
 // Do the following using loops:
 
 // Using `console.log` log all the values from 1 to 10.
-for (let i = 0 ; i = 10 ; i = i+ 1); {
+for (let i = 1 ; i <= 10 ; i++) {
     console.log(i);
 }
 
 // Using `console.log` log all the evan values from 1 to 10.
-for (let i = 0; i < 10; i = i + 1){
+for (let i = 1; i <= 10; i++) {
     if(i % 2 == 0){
-        comsole.log(i);
+        console.log(i, 'even');
     }
 }
 
 // Using `console.log` log all the od values from 1 to 10.
-for (i = 0; i < 10; i = i + 1){
+for (i = 1; i <= 10; i++) {
     if(i % 2 !== 0){
-        console.log(i);
+        console.log(i, 'odd');
     }
 }
 
 // Calculate the sum of all numbers from 1 to 10.
-for (i = 1; i = 10; i = i + 1){
-    if(i + 1 == 10){
-        console.log(i);
+let sum = 0
+for (i = 1; i <= 10; i++) {
+    sum += i
     }
-}
+    console.log(sum);
 
 // Log all the values from 1 to 10 using while loop
-for (let i = 1; i = 10; i = i +1);{
-        console.log(i);
+let initialValue = 1
+while(initialValue <= 10){
+    console.log(initialValue);
+    initialValue++
 }
